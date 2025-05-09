cc5f4be0736a6150e7e891cb99443232443f4032
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-09 12:57:50 +0530

    update js

--- 3-Anagram.js
+++ 3-Anagram.js
@@ -1,9 +1,19 @@
-const anagram = function (s, t) {
+const isAnagram1 = function (s, t) {
   s = s.split("").sort().join("");
   t = t.split("").sort().join("");
 
   return s === t;
 };
+isAnagram();
 
 //anagram => ["a","n","a","g","r","a","m"] => [a,a,a,g,m,n,r] =>aaagmnr
 //nagaram => ["n","a","g","a","r","a","m"] => [a,a,a,g,m,n,r] =>aaagmnr
+
+const isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let i = 0; i < s.length; i++) {}
+};
