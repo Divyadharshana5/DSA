9d012777e4fa42998e43e49a691753829caf00f7
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-09 11:58:03 +0530

    update js

--- Palindrome.js
+++ Palindrome.js
@@ -1,6 +1,7 @@
 const isPalindrome = function (x) {
-  return x === x.toString().split("");
+  return x === x.toString().split("").reverse().join("");
 };
 
 //toString => 121="121"
-//split 121 => ["1","1"]
+//split 121 => ["1","2","1"]
+//x === x.toString().split("").reverse() => 121="121" =>["1","2","1"] =>["1","2","1"] =>"121"
