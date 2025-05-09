067830dad75cb22db4d44af9ada1918eb565e784
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-09 12:52:16 +0530

    update js

--- 3-Anagram.js
+++ 3-Anagram.js
@@ -1,3 +1,9 @@
-const anagram = function () {
-  s = s.split();
+const anagram = function (s, t) {
+  s = s.split("").sort().join("");
+  t = t.split("").sort().join("");
+
+  return s === t;
 };
+
+//anagram => ["a","n","a","g","r","a","m"] => [a,a,a,g,m,n,r] =>aaagmnr
+//nagaram => ["n","a","g","a","r","a","m"] => [a,a,a,g,m,n,r] =>aaagmnr
