ab19854f1a399cbfb02a1de5f0c988fbd650f1fb
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-10 10:20:21 +0530

    using js objects

--- 4-Two_sum.js
+++ 4-Two_sum.js
@@ -9,4 +9,15 @@ const twoSum1 = function (nums, target) {
 
 const twoSum = function (nums, target) {
   var obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    var n = nums[i];
+
+    if (obj[target - n] >= 0) {
+      return [obj[target - n], i];
+    } else {
+      obj[n] = i;
+    }
+  }
 };
+console.log(twoSum([2, 7, 11, 15], 26));
