651fa7d97bb949ff9003c12dcd571186afc75cb1
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-10 10:28:47 +0530

    update Best-time-to-buy-sell-stocks.js

--- 5-Best-time-to-buy-sell-stocks.js
+++ 5-Best-time-to-buy-sell-stocks.js
@@ -1,5 +1,9 @@
 function maxProfit(prices) {
   let globalProfit = 0;
 
-  for (let i = 0; i < prices.length - 1; i++) {}
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      const currentProfit = prices[j] - prices[i];
+    }
+  }
 }
